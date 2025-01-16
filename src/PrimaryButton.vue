9e8715df0137e58d17a18533ccccb245005fdcd9
shuami-dev <shuami.dev@gmail.com>
2025-01-16 08:19:03 +0800

    Update button properties

--- src/PrimaryButton.vue
+++ src/PrimaryButton.vue
@@ -29,29 +29,12 @@ export default defineComponent({
 .primary-button {
   background-color: #4c83fc;
   border-radius: 100px;
-  /* border: 1px solid rgba(32, 4, 175, 0.75); */
-  /* box-shadow: 0 3px 6px rgba(199, 199, 199, 0.7); */
   color: white;
-  font-size: 12px;
+  font-size: 13px;
   font-weight: 600;
   height: 40px;
-  /* padding:  0 0.938rem; */
   padding: 10px 24px;
-  /* transition: background-color 0.3s ease, box-shadow 0.3s ease; */
 }
-/* .primary-button:hover {
-  background-color: #0364cb;
-  box-shadow: 0 3px 6px rgba(9, 113, 199, 0.8);
-}
-.primary-button:active {
-  background-color: #004a9e;
-  box-shadow: 0 2px 4px rgba(9, 106, 186, 0.9);
-  transform: scale(0.98);
-}
-.primary-button:focus {
-  outline: none;
-  box-shadow: 0 0 0 3px rgba(9, 106, 186, 0.5);
-} */
 .primary-button:disabled {
   background-color: #d3d3d3;
   color: #888;
--- src/WarningButton.vue
+++ src/WarningButton.vue
@@ -29,28 +29,12 @@ export default defineComponent({
 .warning-button {
   background-color: #fcaa4c;
   border-radius: 100px;
-  /* border: 1px solid rgba(173, 130, 0, 0.75); */
-  /* box-shadow: 0 3px 6px rgba(199, 199, 199, 0.7); */
   color: black;
-  font-size: 12px;
+  font-size: 13px;
   font-weight: 600;
   height: 40px;
-  padding:  0 0.938rem;
-  /* transition: background-color 0.3s ease, box-shadow 0.3s ease; */
+  padding: 10px 24px;
 }
-/* .warning-button:hover {
-  background-color: rgb(255, 193, 7);
-  box-shadow: 0 3px 6px rgba(147, 123, 2, 0.7);
-}
-.warning-button:active {
-  background-color: rgb(251, 140, 0);
-  box-shadow: 0 2px 4px rgba(253, 224, 71, 0.9);
-  transform: scale(0.98);
-}
-.warning-button:focus {
-  outline: none;
-  box-shadow: 0 0 0 3px rgba(255, 193, 7, 0.5);
-} */
 .warning-button:disabled {
   background-color: #d3d3d3;
   color: #888;
